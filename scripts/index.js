68882fee238f44ea0d8394af37a21428f9bf0373
VadSmith <oldfatham@gmail.com>
2021-12-04 21:33:16 +0700

    feat: form fix

--- scripts/index.js
+++ scripts/index.js
@@ -31,63 +31,61 @@ const initialCards = [
   }
 ];
 
-const popup = document.querySelector(".popup");
-const popupCloseButton = popup.querySelector(".popup__close-button");
-const editButton = document.querySelector(".profile__edit-button");
+const popupAdd = document.querySelector(".popup_type_add");
+const popupAddCloseButton = popupAdd.querySelector('.popup__close-button');
 const addButton = document.querySelector('.profile__add-button');
+const formAddElement = document.querySelector('.popup__form_type_add');
+const formAddPlace = formAddElement.querySelector('.popup__input_type_name');
+const formAddLink = formAddElement.querySelector('.popup__input_type_link');
+
+const templateEl = document.querySelector('.template-element');
+let elementsContainer = document.querySelector('.elements');
+
 const profileName = document.querySelector('.profile__name')
 const profileJob = document.querySelector('.profile__occupation');
-const templateEl = document.querySelector('.template-element');
-const elementsContainer = document.querySelector('.elements');
-// const popupOverlay = popup.querySelector('.popup__overlay');
+const popupEdit = document.querySelector(".popup_type_edit");
+const popupEditCloseButton = popupEdit.querySelector(".popup__close-button");
+const editButton = document.querySelector(".profile__edit-button");
+let formEditElement = popupEdit.querySelector('.popup__form');
+let nameInput = formEditElement.querySelector('.popup__input_type_name');
+let jobInput = formEditElement.querySelector('.popup__input_type_job');
 
-// Находим форму в DOM
-let formElement = popup.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__input_type_name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__input_type_job'); // Воспользуйтесь инструментом .querySelector()
-
-function openPopup() {
+function openPopup(popup) {
   popup.classList.add("popup_opened");
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+  if (popup.className.includes('popup_type_edit')) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  }
 }
-function closePopup() {
+
+function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
-editButton.addEventListener('click', openPopup);
-addButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-// popupOverlay.addEventListener('click', closePopup);
-// popup.addEventListener('click', closePopup);
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Получите значение полей jobInput и nameInput из свойства value
-
-  // Выберите элементы, куда должны быть вставлены значения полей
+function formEditSubmitHandler(evt) {
+  evt.preventDefault();
   profileJob.textContent = jobInput.value;
   profileName.textContent = nameInput.value;
-  // Вставьте новые значения с помощью textContent
-  closePopup();
+  closePopup(popupEdit);
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-
-
+function formAddSubmitHandler(evt) {
+  evt.preventDefault();
+  const card = {
+    name: place.value,
+    link: link.value,
+    alt: place.value
+  }
+  elementsContainer.prepend(getElement(card));
+  formAddPlace = '';
+  formAddLink = '';
+  closePopup(popupAdd);
+}
 
 function render() {
   const elementsHTML = initialCards.map((card) => {
-    console.log(card);
-
     return getElement(card);
   });
   elementsContainer.append(...elementsHTML);
@@ -97,12 +95,18 @@ function getElement(card) {
   const newCard = templateEl.content.cloneNode(true);
   const photo = newCard.querySelector('.element__photo');
   const title = newCard.querySelector('.element__title');
-
   photo.setAttribute('src', card.link);
   photo.setAttribute('alt', card.alt);
   title.textContent = card.name;
-
   return newCard;
 }
+
+editButton.addEventListener('click', () => { openPopup(popupEdit); });
+formEditElement.addEventListener('submit', formEditSubmitHandler);
+popupEditCloseButton.addEventListener('click', () => { closePopup(popupEdit); });
+
+addButton.addEventListener('click', () => { openPopup(popupAdd); })
+formAddElement.addEventListener('submit', formAddSubmitHandler);
+popupAddCloseButton.addEventListener('click', () => { closePopup(popupAdd); });
 
 render();
